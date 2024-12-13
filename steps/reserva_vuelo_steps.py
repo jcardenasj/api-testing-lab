df8090e567ab22229e5bebf1cf4d80359ce7f4f3
Juan José Cárdenas <cardenas95@gmail.com>
2024-12-13 16:25:48 +0000

    reserva vuelo py

---
+++ steps/reserva_vuelo_steps.py
@@ -0,0 +1,30 @@
+from behave import given, when, then
+
+@given(u'un ID de vuelo seleccionado "23"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given un ID de vuelo seleccionado "23"')
+
+
+@when(u'usuario envia una nueva solicitud de reserva')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When usuario envia una nueva solicitud de reserva')
+
+
+@then(u'retornar mensaje de cancelación por vuelo completamente reservado')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then retornar mensaje de cancelación por vuelo completamente reservado')
+
+
+@given(u'un ID de vuelo seleccionado "24"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given un ID de vuelo seleccionado "24"')
+
+
+@when(u'usuario envia una solicitud de reserva')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When usuario envia una solicitud de reserva')
+
+
+@then(u'devolver mensaje de información con nuevo precio de reserva')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then devolver mensaje de información con nuevo precio de reserva')
